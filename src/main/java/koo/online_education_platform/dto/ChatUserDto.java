5ab326535dc2e87f870c7c3f16ffd75576538a2a
rngustj9139 <mnb9139@khu.ac.kr>
2025-05-06 07:44:36 +0900

    Update:ChatUserDto comment

--- src/main/java/koo/online_education_platform/dto/ChatUserDto.java
+++ src/main/java/koo/online_education_platform/dto/ChatUserDto.java
@@ -1,24 +1,20 @@
 package koo.online_education_platform.dto;
 
 import koo.online_education_platform.entity.ChatUser;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatUserDto { // 로그인시 사용되는 DTO
 
-    /* TODO ChatUser 엔티티에 따라서 전체 뜯어 고치기 */
-    private Long id; // DB 저장되는 id : PK
-    private String nickName; // 소셜에서 제공받은 유저명 => 유저 닉네임
+    private Long id; // DB 저장되는 id: PK
+    private String nickName; // 소셜에서 제공받은 유저명: 유저 닉네임
     private String email; // 소셜에서 제공받은 이메일
-    private String provider; // 소셜 제공자 -> ex) 네이버, 카카오 ----
+    private String provider; // 소셜 제공자: e.g. 네이버, 카카오 etc
 
-    public static ChatUserDto of(ChatUser chatUserEntity) {
+    public static ChatUserDto of (ChatUser chatUserEntity) { // ChatUserDto dto = ChatUserDto.of(chatUser);
         ChatUserDto chatUserDto = ChatUserDto.builder()
                 .id(chatUserEntity.getId())
                 .nickName(chatUserEntity.getNickName())
