6a7a0a0e6484a04c6d427e0edd762ae0cd0e8d8b
rngustj9139 <mnb9139@khu.ac.kr>
2025-05-06 13:54:07 +0900

    Update:ChatRepository comment

--- src/main/java/koo/online_education_platform/dto/WebSocketMessage.java
+++ src/main/java/koo/online_education_platform/dto/WebSocketMessage.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * WebRTC 연결 시 사용되는 클래스로 초기 연결 정보를 주고 받을 때 사용된다.
+ * WebRTC 연결 시 사용되는 클래스로 초기 연결 정보를 교환할 때 사용된다.
  **/
 @Data
 @AllArgsConstructor
@@ -17,7 +17,7 @@ public class WebSocketMessage {
     private String data; // roomId
     private Object candidate; // 상태
     /*
-     * WebRTC에서는 두 브라우저(혹은 클라이언트)가 영상, 오디오, 데이터 채널로 통신하려면 다음과 같은 정보를 서로 알아야 한다.
+     * WebRTC에서는 두 브라우저(혹은 클라이언트)가 영상, 오디오, 데이터 채널로 통신하려면 다음과 같은 정보를 서로 알아야 하고 아래의 모든 정보를 담는 것이 SDP이다.
      * 내가 사용할 코덱 종류 (예: VP8, H264, Opus 등)
      * 사용할 IP 주소와 포트
      * 암호화 방식
--- src/main/java/koo/online_education_platform/repository/ChatRepository.java
+++ src/main/java/koo/online_education_platform/repository/ChatRepository.java
@@ -2,49 +2,45 @@ package koo.online_education_platform.repository;
 
 import koo.online_education_platform.dto.ChatRoomDto;
 import koo.online_education_platform.service.fileService.FileService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
 
-/**
- * 추후 DB 와 연결 시 Service 와 Repository(DAO) 로 분리 예정
- */
 @Repository
 @Slf4j
+@RequiredArgsConstructor
 public class ChatRepository {
 
     private Map<String, ChatRoomDto> chatRoomMap;
+    private final FileService fileService; // 채팅방 삭제에 따른 채팅방의 사진 삭제를 위한 fileService 주입
 
     @PostConstruct
     private void init() {
         chatRoomMap = new LinkedHashMap<>();
     }
 
-    // 채팅방 삭제에 따른 채팅방의 사진 삭제를 위한 fileService 주입
-    @Autowired
-    FileService fileService;
-
     // 전체 채팅방 조회
-    public List<ChatRoomDto> findAllRoom(){
+    public List<ChatRoomDto> findAllRoom() {
         // 채팅방 생성 순서를 최근순으로 반환
-        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        List chatRooms = new ArrayList<>(chatRoomMap.values()); // List: 인터페이스 (추상체), ArrayList: 클래스 (구현체)
         Collections.reverse(chatRooms);
 
         return chatRooms;
     }
 
     // roomID 기준으로 채팅방 찾기
-    public ChatRoomDto findRoomById(String roomId){
+    public ChatRoomDto findRoomById(String roomId) {
         return chatRoomMap.get(roomId);
     }
 
     // roomName 로 채팅방 만들기
-    public ChatRoomDto createChatRoom(String roomName, String roomPwd, boolean secretChk, int maxUserCnt){
+    public ChatRoomDto createChatRoom(String roomName, String roomPwd, boolean secretChk, int maxUserCnt) {
         // roomName 와 roomPwd 로 chatRoom 빌드 후 return
-
         ChatRoomDto chatRoomDto = ChatRoomDto.builder()
                 .roomId(UUID.randomUUID().toString())
                 .roomName(roomName)
@@ -55,19 +51,19 @@ public class ChatRepository {
                 .maxUserCnt(maxUserCnt) // 최대 인원수 제한
                 .build();
 
-        // map 에 채팅룸 아이디와 만들어진 채팅룸을 저장장
+        // map 에 채팅룸 아이디와 만들어진 채팅룸을 저장
         chatRoomMap.put(chatRoomDto.getRoomId(), chatRoomDto);
 
         return chatRoomDto;
     }
 
-    // 채팅방 인원+1
-    public void plusUserCnt(String roomId){
+    // 채팅방 인원 + 1
+    public void plusUserCnt(String roomId) {
         ChatRoomDto room = chatRoomMap.get(roomId);
         room.setUserCount(room.getUserCount() + 1);
     }
 
-    // 채팅방 인원-1
+    // 채팅방 인원 - 1
     public void minusUserCnt(String roomId){
         ChatRoomDto room = chatRoomMap.get(roomId);
         room.setUserCount(room.getUserCount() - 1);
@@ -87,7 +83,7 @@ public class ChatRepository {
     }
 
     // 채팅방 유저 리스트에 유저 추가
-    public String addUser(String roomId, String userName){
+    public String addUser(String roomId, String userName) {
         ChatRoomDto room = chatRoomMap.get(roomId);
         String userUUID = UUID.randomUUID().toString();
 
@@ -98,42 +94,40 @@ public class ChatRepository {
     }
 
     // 채팅방 유저 이름 중복 확인
-    public String isDuplicateName(String roomId, String username){
+    public String isDuplicateName(String roomId, String username) {
         ChatRoomDto room = chatRoomMap.get(roomId);
         String tmp = username;
 
-        // 만약 userName 이 중복이라면 랜덤한 숫자를 붙임
-        // 이때 랜덤한 숫자를 붙였을 때 getUserlist 안에 있는 닉네임이라면 다시 랜덤한 숫자 붙이기!
-        while(room.getUserList().containsValue(tmp)){
+        // 만약 userName이 중복이라면 랜덤한 숫자를 붙임
+        // 랜덤한 숫자를 붙였을 때 getUserlist 내부에 존재하는 닉네임이라면 다시 해당 작업 반복 수행
+        while (room.getUserList().containsValue(tmp)) {
             int ranNum = (int) (Math.random() * 100) + 1;
-
-            tmp = username + ranNum;
+            tmp = username + ranNum; // String에 int 값을 더하는 경우 자바가 자동으로 문자열로 변환 수행
         }
 
         return tmp;
     }
 
-    // 채팅방 유저 리스트 삭제
-    public void delUser(String roomId, String userUUID){
+    // 채팅방에서 유저 삭제
+    public void delUser(String roomId, String userUUID) {
         ChatRoomDto room = chatRoomMap.get(roomId);
         room.getUserList().remove(userUUID);
     }
 
     // 채팅방 userName 조회
-    public String getUserName(String roomId, String userUUID){
+    public String getUserName(String roomId, String userUUID) {
         ChatRoomDto room = chatRoomMap.get(roomId);
 
         return (String) room.getUserList().get(userUUID);
     }
 
-    // 채팅방 전체 userlist 조회
-    public ArrayList<String> getUserList(String roomId){
+    // 채팅방 전체 userList 조회
+    public ArrayList<String> getUserList(String roomId) {
         ArrayList<String> list = new ArrayList<>();
 
         ChatRoomDto room = chatRoomMap.get(roomId);
 
-        // hashmap 을 for 문을 돌린 후
-        // value 값만 뽑아내서 list 에 저장 후 reutrn
+        // 해쉬맵을 순차 접근하여 value 값만 뽑아내고, list에 저장 후 return
         room.getUserList().forEach((key, value) -> list.add((String) value));
 
         return list;
@@ -141,8 +135,6 @@ public class ChatRepository {
 
     // 채팅방 비밀번호 조회
     public boolean confirmPwd(String roomId, String roomPwd) {
-        // String pwd = chatRoomMap.get(roomId).getRoomPwd();
-
         return roomPwd.equals(chatRoomMap.get(roomId).getRoomPwd());
     }
 
@@ -151,13 +143,12 @@ public class ChatRepository {
         try {
             // 채팅방 삭제
             chatRoomMap.remove(roomId);
-
-            // 채팅방 안에 있는 파일 삭제
+            // 채팅방 내부에 존재하는 파일 삭제
             fileService.deleteFileDir(roomId);
 
             log.info("삭제 완료 roomId : {}", roomId);
-        } catch (Exception e) { // 만약에 예외 발생시 확인하기 위해서 try catch
-            System.out.println(e.getMessage());
+        } catch (Exception e) { // 만약 예외 발생시 에러 로그를 확인
+            log.error(e.getMessage());
         }
     }
 
