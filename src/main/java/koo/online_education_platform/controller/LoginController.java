d6add7ff51db8e67ac17f94cdd4939a2344b730e
rngustj9139 <mnb9139@khu.ac.kr>
2025-05-10 03:50:23 +0900

    Update:chatlogin (Add method, action to form)

--- src/main/java/koo/online_education_platform/controller/LoginController.java
+++ src/main/java/koo/online_education_platform/controller/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class LoginController { // SecurityConfig 확인 (Spring Security를 활용한 로그인 구현 완료)
 
     @GetMapping("/chatlogin")
     public String goLogin(){
